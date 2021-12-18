6e816a4ee669bc2e177bc396c2590b0309eb0f27
Pablo Aldape <pablo@vest.io>
2021-12-18 15:53:46 -0500

    d6 p1 improvement + p2

--- d6/p1.py
+++ d6/p1.py
@@ -1,19 +1,30 @@
 def read(filename):
     with open(filename) as f:
-        return list(map(int, f.readline().strip().split(",")))
+        age_list = map(int, f.readline().strip().split(","))
+        age_counts = {age: 0 for age in range(9)}
+        for age in age_list:
+            age_counts[age] += 1
+        return age_counts
 
 
-def step(ages):
-    for i in range(len(ages)):
-        if ages[i] > 0:
-            ages[i] -= 1
+# Lame iterative solution. This can be done mathematically
+def step(age_counts):
+    new_counts = {age: 0 for age in range(9)}
+    for age, count in age_counts.items():
+        if age == 0:
+            new_counts[6] += count
+            new_counts[8] += count
         else:
-            ages[i] = 6
-            ages.append(8)
+            new_counts[age - 1] += count
+    return new_counts
+
+
+def solve(age_counts, days):
+    for _ in range(days):
+        age_counts = step(age_counts)
+    return sum(age_counts.values())
 
 
 if __name__ == "__main__":
-    ages = read("input.txt")
-    for _ in range(80):
-        step(ages)
-    print(len(ages))
+    age_counts = read("input.txt")
+    print(solve(age_counts, 80))
---
+++ d6/p2.py
@@ -0,0 +1,6 @@
+from p1 import read, solve
+
+
+if __name__ == "__main__":
+    age_counts = read("input.txt")
+    print(solve(age_counts, 256))
