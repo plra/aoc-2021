ee783856f56fd1c7298f8609227584f4dcc61eee
Pablo Aldape <pablo.aldape@gmail.com>
2021-12-19 12:36:00 -0500

    rf/cmts for d12

--- d12/p1.py
+++ d12/p1.py
@@ -12,18 +12,19 @@ def read(filename):
 
 # Do it manually with a stack
 def find_paths(G, start="start", end="end"):
+    # Stack of (incomplete) paths from `start` towards `end`
     path_stack = [[start]]
     complete_paths = []
     while path_stack:
-        cur_path = path_stack.pop()
-        cur_cave = cur_path[-1]
+        path = path_stack.pop()
+        cur_cave = path[-1]
         if cur_cave == end:
-            complete_paths.append(cur_path)
+            complete_paths.append(path)
         else:
             for nxt_cave in G[cur_cave]:
-                if nxt_cave[0].isupper() or nxt_cave not in cur_path:
+                if nxt_cave[0].isupper() or nxt_cave not in path:
                     # We can visit a cave only if it's large or unseen
-                    path_stack.append(cur_path + [nxt_cave])
+                    path_stack.append(path + [nxt_cave])
     return complete_paths
 
 
--- d12/p2.py
+++ d12/p2.py
@@ -2,27 +2,27 @@ from p1 import read
 
 
 def find_paths(G, start="start", end="end"):
-    # Stack containing pairs (incomplete) paths from start towards end along with an indicator of
-    # whether that path contains 2 visits of the same small cave
+    # Stack of pairs `(path, retread)`, where `path` is an (incomplete) path from `start` towards
+    # `end` and `retread` indicates whether `path` visits the same small cave twice
     path_stack = [([start], False)]
     complete_paths = []
     while path_stack:
-        cur_path, visited_2_small_caves = path_stack.pop()
-        cur_cave = cur_path[-1]
+        path, retread = path_stack.pop()
+        cur_cave = path[-1]
         if cur_cave == end:
-            complete_paths.append(cur_path)
+            complete_paths.append(path)
         else:
             for nxt_cave in G[cur_cave]:
                 if nxt_cave[0].isupper():
                     # We can visit large caves as much as we like
-                    path_stack.append((cur_path + [nxt_cave], visited_2_small_caves))
-                elif nxt_cave not in cur_path:
+                    path_stack.append((path + [nxt_cave], retread))
+                elif nxt_cave not in path:
                     # We can visit new small caves as much as we like (note start can't be new)
-                    path_stack.append((cur_path + [nxt_cave], visited_2_small_caves))
-                elif not visited_2_small_caves and nxt_cave != start:
+                    path_stack.append((path + [nxt_cave], retread))
+                elif not retread and nxt_cave != start:
                     # We can visit a small cave we've already seen as long as we haven't already
                     # visited 2 small caves, and that small cave isn't start
-                    path_stack.append((cur_path + [nxt_cave], True))
+                    path_stack.append((path + [nxt_cave], True))
     return complete_paths
 
 
