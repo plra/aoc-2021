e2706a564dce8f3ee16ff9ab600344db27e8a7ae
Pablo Aldape <pablo.aldape@gmail.com>
2021-12-18 23:33:32 -0500

    rm non-padded day numbers

--- d5/p2.py
+++
@@ -1,8 +0,0 @@
-from p1 import read, add
-
-if __name__ == "__main__":
-    lines = read("input.txt")
-    hits = {}
-    for x1, y1, x2, y2 in lines:
-        add(x1, y1, x2, y2, hits)
-    print(sum(n > 1 for n in hits.values()))
--- d6/p2.py
+++
@@ -1,6 +0,0 @@
-from p1 import read, solve
-
-
-if __name__ == "__main__":
-    age_counts = read("input.txt")
-    print(solve(age_counts, 256))
