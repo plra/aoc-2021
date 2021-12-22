c4dfd9ed2b50932c72d6b552444562996246bb41
Pablo Aldape <pablo.aldape@gmail.com>
2021-12-21 21:28:27 -0500

    d01 1-liners

--- d01/p1.py
+++ d01/p1.py
@@ -1,4 +1,8 @@
-with open("input_0.txt") as f:
-    depths = [int(line.strip()) for line in f.readlines()]
-    print(sum(depths[i] > depths[i - 1] for i in range(1, len(depths))))
+print(
+    sum(
+        xs[i] > xs[i - 1]
+        for xs in [list(map(int, open("input.txt").readlines()))]
+        for i in range(1, len(xs))
+    )
+)
 
--- d01/p2.py
+++ d01/p2.py
@@ -1,10 +1,8 @@
-with open("input.txt") as f:
-    depths = [int(line.strip()) for line in f.readlines()]
+print(
+    sum(
+        xs[i] > xs[i - 3]
+        for xs in [list(map(int, open("input.txt").readlines()))]
+        for i in range(3, len(xs))
+    )
+)
 
-    n_increases = 0
-    for i in range(3, len(depths)):
-        cur_sum = depths[i] + depths[i - 1] + depths[i - 2]
-        prv_sum = depths[i - 1] + depths[i - 2] + depths[i - 3]
-        n_increases += cur_sum > prv_sum
-
-    print(n_increases)
