4ddd7d64aa6a9a3165ae9c7f81afc6b3e4282eac
Pablo Aldape <pablo.aldape@gmail.com>
2021-12-21 23:06:17 -0500

    d03 p2 1-liner improvement

--- d03/p2.py
+++ d03/p2.py
@@ -3,16 +3,17 @@ from operator import ge, lt
 
 print(
     reduce(
-        lambda x, y: x * int(y.pop(), 2),
+        lambda p, b: p * int(b.pop(), 2),
         [
             reduce(
                 lambda sets, i: [
-                    {
-                        s
-                        for s in sets[r]
-                        if int(s[i])
-                        == (f(sum(int(s[i]) for s in sets[r]), len(sets[r]) / 2))
-                    }
+                    set(
+                        filter(
+                            lambda s: int(s[i])
+                            == (f(sum(int(s[i]) for s in sets[r]), len(sets[r]) / 2)),
+                            sets[r],
+                        )
+                    )
                     or sets[r]
                     for r, f in [(0, ge), (1, lt)]
                 ],
